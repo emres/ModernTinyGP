2634d7e02b364d2da9d7193385666ab3e14096d4
Emre Sevinç <emre.sevinc@gmail.com>
2024-02-12 16:35:31 +0100

    Add the first unit test for one happy path

---
+++ src/test/java/be/tmdata/ModernTinyGP/ModernTinyGPTest.java
@@ -0,0 +1,54 @@
+package be.tmdata.ModernTinyGP;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class ModernTinyGPTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void setupFitness_WithValidFileName_ShouldReadDataFromFileAndSetVariables() {
+        double delta = 0.0001;
+        long randomizationSeed = -1;
+        String fileName = "src/test/resources/valid_sin_data.txt";
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        ModernTinyGP gp = new ModernTinyGP(fileName, randomizationSeed);
+
+        gp.setupFitness(fileName);
+
+        assertEquals(1, ModernTinyGP.varNumber);
+        assertEquals(100, ModernTinyGP.randomNumber);
+        assertEquals(-5, ModernTinyGP.minRandom, delta);
+        assertEquals(5, ModernTinyGP.maxRandom, delta);
+        assertEquals(63, ModernTinyGP.fitnessCases);
+
+        // Verify console output
+        assertEquals("", getConsoleOutput(outContent)); // Assuming no error messages are printed
+    }
+
+    // Helper method to capture console output
+    private String getConsoleOutput(ByteArrayOutputStream outContent) {
+        return outContent.toString();
+    }
+
+}
